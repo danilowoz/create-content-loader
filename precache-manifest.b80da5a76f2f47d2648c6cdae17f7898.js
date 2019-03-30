bb0c17907247734f82b30bcea213fe51710a0eae
Danilo Woznica <danilowoz@gmail.com>
2019-03-30 17:38:03 +0000

    Updates

---
+++ precache-manifest.b80da5a76f2f47d2648c6cdae17f7898.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff11d844cfb16f330f7c590b88adb17",
+    "url": "/create-content-loader/static/media/clone.1ff11d84.svg"
+  },
+  {
+    "revision": "b246e64ec53c24dced6d",
+    "url": "/create-content-loader/static/css/main.cb509da2.chunk.css"
+  },
+  {
+    "revision": "605b7644e09a0b89d57f",
+    "url": "/create-content-loader/static/js/runtime~main.f7efc1a2.js"
+  },
+  {
+    "revision": "8e19265a8c6a0c82b08a",
+    "url": "/create-content-loader/static/js/2.21cf03ff.chunk.js"
+  },
+  {
+    "revision": "e480dbf597689bcc099fe3cc6fae898e",
+    "url": "/create-content-loader/static/media/select.e480dbf5.svg"
+  },
+  {
+    "revision": "7a4373e35fd80b36d8d0f09c9611ed54",
+    "url": "/create-content-loader/static/media/trash.7a4373e3.svg"
+  },
+  {
+    "revision": "b246e64ec53c24dced6d",
+    "url": "/create-content-loader/static/js/main.2c29921e.chunk.js"
+  },
+  {
+    "revision": "7ddb0a9acb70f7af1e6db9ed4b11279e",
+    "url": "/create-content-loader/static/media/rect.7ddb0a9a.svg"
+  },
+  {
+    "revision": "23e0a9a0a51cd0ffa42c8fa4e6d3939a",
+    "url": "/create-content-loader/static/media/circle.23e0a9a0.svg"
+  },
+  {
+    "revision": "6eaae66534112014ca62db29d14b241a",
+    "url": "/create-content-loader/static/media/react.6eaae665.svg"
+  },
+  {
+    "revision": "22efb7c22774c39c790448f62bda240d",
+    "url": "/create-content-loader/static/media/vue.22efb7c2.svg"
+  },
+  {
+    "revision": "08cd2fc5e47c4442f4cf51376dfc5fa8",
+    "url": "/create-content-loader/static/media/grid.08cd2fc5.svg"
+  },
+  {
+    "revision": "c96db7042df107a55000d410e9c3b1bf",
+    "url": "/create-content-loader/index.html"
+  }
+];
