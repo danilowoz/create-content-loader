666ac9e2d901a97bec69549356d805f455b971dd
Danilo Woznica <danilowoz@gmail.com>
2019-03-30 16:16:07 +0000

    chore(commit flow): add commitlint and semantic release

--- src/Gallery/insertYourLoaderHere/image-list.js
+++ src/Gallery/insertYourLoaderHere/image-list.js
@@ -4,56 +4,59 @@ import ContentLoader from "react-content-loader"
 const Loader = props => {
   let height, width
   switch (props.screen) {
-    case 'mobile' : {
-      height='100'
-      width='400'
-      break;
+    case "mobile": {
+      height = "100"
+      width = "400"
+      break
     }
-    case 'desktop' : {
-      height='100'
-      width='1060'
-      break;
+    case "desktop": {
+      height = "100"
+      width = "1060"
+      break
     }
-    case 'large-screen' : {
-      height='150'
-      width='1920'
-      break;
+    case "large-screen": {
+      height = "150"
+      width = "1920"
+      break
     }
-    default : {
-      height='100'
-      width='1060'
-      break;
+    default: {
+      height = "100"
+      width = "1060"
+      break
     }
   }
   return (
-    <ContentLoader 
+    <ContentLoader
       height={height}
       width={width}
       speed={2}
-      primaryColor='#f3f3f3'
-      secondaryColor='#ecebeb'
-      {...props}>
-      {
-        (props.imageType === 'circle')
-        ? <circle cx='60' cy='45' r='30' />
-        : <rect x='20' y='20' rx='5' ry='5' width='64' height='63' /> 
-      }
-      <rect x='105' y='20' rx='5' ry='5' width='250' height='12' />
-      <rect x='105' y='40' rx='5' ry='5' width='180' height='12' />
-      <rect x='105' y='60' rx='5' ry='5' width='125' height='12' />
+      primaryColor="#f3f3f3"
+      secondaryColor="#ecebeb"
+      {...props}
+    >
+      {props.imageType === "circle" ? (
+        <circle cx="60" cy="45" r="30" />
+      ) : (
+        <rect x="20" y="20" rx="5" ry="5" width="64" height="63" />
+      )}
+      <rect x="105" y="20" rx="5" ry="5" width="250" height="12" />
+      <rect x="105" y="40" rx="5" ry="5" width="180" height="12" />
+      <rect x="105" y="60" rx="5" ry="5" width="125" height="12" />
     </ContentLoader>
   )
 }
 
 const ImageList = () => (
   <React.Fragment>
-    {
-      Array(5)
+    {Array(5)
       .fill("")
       .map((e, i) => (
-        <Loader screen='desktop' key={i} style={{ opacity: Number(2 / i).toFixed(1) }} />
-      ))
-    }
+        <Loader
+          screen="desktop"
+          key={i}
+          style={{ opacity: Number(2 / i).toFixed(1) }}
+        />
+      ))}
   </React.Fragment>
 )
 
