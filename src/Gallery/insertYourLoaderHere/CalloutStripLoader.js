42f6304a24fd19b114b9cf48263210dc3b3e3e5e
Deepak Kumar Vellingiri <aspiring.us@gmail.com>
2019-09-23 14:26:57 +0530

    Update index.js (#55)
    Added CalloutStripLoader Example

--- src/Gallery/insertYourLoaderHere/CalloutStripLoader.js
+++ src/Gallery/insertYourLoaderHere/CalloutStripLoader.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
 const CalloutStripLoader = props => {
   return (
@@ -11,10 +11,10 @@ const CalloutStripLoader = props => {
       secondaryColor="#ecebeb"
       {...props}
     >
-      <rect height='5.5' rx='1' ry='1' width='340' x='31' y='5' />
-      <rect height='5.5' rx='1' ry='1' width='340' x='31' y='15' />
-      <circle cx='388' cy='12' r='12' />
-      <rect height='24' rx='0' ry='0' width='24' x='0' y='0' />
+      <rect height="5.5" rx="1" ry="1" width="340" x="31" y="5" />
+      <rect height="5.5" rx="1" ry="1" width="340" x="31" y="15" />
+      <circle cx="388" cy="12" r="12" />
+      <rect height="24" rx="0" ry="0" width="24" x="0" y="0" />
     </ContentLoader>
   )
 }
@@ -23,7 +23,7 @@ CalloutStripLoader.metadata = {
   name: 'Deepak Kumar Vellingiri', // My name
   github: 'dkvellin', // Github username
   description: 'This is a sample loader of the callout strip', // Little tagline
-  filename: 'CalloutStripLoader' // filename of your loader
+  filename: 'CalloutStripLoader', // filename of your loader
 }
 
 export default CalloutStripLoader
--- src/Gallery/insertYourLoaderHere/index.js
+++ src/Gallery/insertYourLoaderHere/index.js
@@ -1,3 +1,4 @@
+export { default as CalloutStripLoader } from './CalloutStripLoader'
 export { default as Card } from './Card'
 export { default as AvatarWithText } from './AvatarWithText'
 export { default as HomePage } from './HomePage'
