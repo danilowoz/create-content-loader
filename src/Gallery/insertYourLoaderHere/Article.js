9f0a399741d49cb7cb3f0fe014dc40cc4f1add92
Royal Bhati <royal240596@gmail.com>
2019-09-29 07:56:50 -0400

    Article with image and content style

---
+++ src/Gallery/insertYourLoaderHere/Article.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const Article = () => (
+  <ContentLoader
+    height={160}
+    width={400}
+    speed={1}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <rect x="0" y="13" rx="4" ry="4" width="400" height="9" />
+    <rect x="0" y="29" rx="4" ry="4" width="100" height="8" />
+    <rect x="0" y="50" rx="4" ry="4" width="400" height="10" />
+    <rect x="0" y="65" rx="4" ry="4" width="400" height="10" />
+    <rect x="0" y="79" rx="4" ry="4" width="100" height="10" />
+    <rect x="0" y="99" rx="5" ry="5" width="400" height="200" />
+  </ContentLoader>
+)
+
+Article.metadata = {
+  name: 'RoyalBhati',
+  github: 'royalbhati',
+  description: 'Simple Article',
+  filename: 'Article',
+}
+
+export default Article
