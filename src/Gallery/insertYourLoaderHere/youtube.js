aa1ece9b13e61382d36503a740205caa8990161e
Danilo Woznica <danilowoz@gmail.com>
2019-09-30 14:03:51 +0100

    fix(gallery): reorder

--- src/Gallery/insertYourLoaderHere/youtube.js
+++ src/Gallery/insertYourLoaderHere/youtube.js
@@ -1,41 +1,39 @@
 import React from 'react'
 import ContentLoader from 'react-content-loader'
 
-import ContentLoader from "react-content-loader"
-
 const Youtube = () => (
-  <ContentLoader 
+  <ContentLoader
     height={507}
     width={900}
     speed={2}
     primaryColor="#f3f3f3"
     secondaryColor="#ecebeb"
   >
-    <rect x="30" y="20" rx="0" ry="0" width="130" height="23" /> 
-    <rect x="30" y="60" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="30" y="189" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="30" y="211" rx="0" ry="0" width="140" height="15" /> 
-    <rect x="243" y="60" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="243" y="189" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="243" y="211" rx="0" ry="0" width="140" height="15" /> 
-    <rect x="455" y="60" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="455" y="189" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="455" y="211" rx="0" ry="0" width="140" height="15" /> 
-    <rect x="667" y="60" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="667" y="188" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="667" y="209" rx="0" ry="0" width="140" height="15" /> 
-    <rect x="30" y="280" rx="0" ry="0" width="130" height="23" /> 
-    <rect x="30" y="320" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="30" y="450" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="30" y="474" rx="0" ry="0" width="140" height="15" /> 
-    <rect x="243" y="320" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="455" y="320" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="667" y="320" rx="0" ry="0" width="200" height="120" /> 
-    <rect x="243" y="450" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="455" y="450" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="667" y="450" rx="0" ry="0" width="200" height="15" /> 
-    <rect x="243" y="474" rx="0" ry="0" width="140" height="15" /> 
-    <rect x="455" y="474" rx="0" ry="0" width="140" height="15" /> 
+    <rect x="30" y="20" rx="0" ry="0" width="130" height="23" />
+    <rect x="30" y="60" rx="0" ry="0" width="200" height="120" />
+    <rect x="30" y="189" rx="0" ry="0" width="200" height="15" />
+    <rect x="30" y="211" rx="0" ry="0" width="140" height="15" />
+    <rect x="243" y="60" rx="0" ry="0" width="200" height="120" />
+    <rect x="243" y="189" rx="0" ry="0" width="200" height="15" />
+    <rect x="243" y="211" rx="0" ry="0" width="140" height="15" />
+    <rect x="455" y="60" rx="0" ry="0" width="200" height="120" />
+    <rect x="455" y="189" rx="0" ry="0" width="200" height="15" />
+    <rect x="455" y="211" rx="0" ry="0" width="140" height="15" />
+    <rect x="667" y="60" rx="0" ry="0" width="200" height="120" />
+    <rect x="667" y="188" rx="0" ry="0" width="200" height="15" />
+    <rect x="667" y="209" rx="0" ry="0" width="140" height="15" />
+    <rect x="30" y="280" rx="0" ry="0" width="130" height="23" />
+    <rect x="30" y="320" rx="0" ry="0" width="200" height="120" />
+    <rect x="30" y="450" rx="0" ry="0" width="200" height="15" />
+    <rect x="30" y="474" rx="0" ry="0" width="140" height="15" />
+    <rect x="243" y="320" rx="0" ry="0" width="200" height="120" />
+    <rect x="455" y="320" rx="0" ry="0" width="200" height="120" />
+    <rect x="667" y="320" rx="0" ry="0" width="200" height="120" />
+    <rect x="243" y="450" rx="0" ry="0" width="200" height="15" />
+    <rect x="455" y="450" rx="0" ry="0" width="200" height="15" />
+    <rect x="667" y="450" rx="0" ry="0" width="200" height="15" />
+    <rect x="243" y="474" rx="0" ry="0" width="140" height="15" />
+    <rect x="455" y="474" rx="0" ry="0" width="140" height="15" />
     <rect x="667" y="474" rx="0" ry="0" width="140" height="15" />
   </ContentLoader>
 )
