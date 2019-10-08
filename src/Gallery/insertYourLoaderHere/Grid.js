be1db06ed701e26e125fb675ef53ab72ad6bcc68
Baptiste Firket <baptistefirket@gmail.com>
2019-10-08 10:51:41 +0200

    Create Grid.js

---
+++ src/Gallery/insertYourLoaderHere/Grid.js
@@ -0,0 +1,31 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Grid = props => {
+  return (
+    <ContentLoader
+      height={450}
+      width={820}
+      speed={2}
+      primaryColor="#d9d9d9"
+      secondaryColor="#ecebeb"
+      {...props}
+    >
+      <rect x="10" y="10" rx="5" ry="5" width="260" height="140" /> 
+      <rect x="280" y="10" rx="5" ry="5" width="260" height="280" /> 
+      <rect x="550" y="10" rx="5" ry="5" width="260" height="140" /> 
+      <rect x="10" y="160" rx="5" ry="5" width="260" height="280" /> 
+      <rect x="280" y="300" rx="5" ry="5" width="260" height="140" /> 
+      <rect x="550" y="160" rx="5" ry="5" width="260" height="280" />
+    </ContentLoader>
+  )
+}
+
+Grid.metadata = {
+  name: 'baptiste fkt',
+  github: 'baptistefkt',
+  description: 'Three column grid layout',
+  filename: 'Grid',
+}
+
+export default Grid
