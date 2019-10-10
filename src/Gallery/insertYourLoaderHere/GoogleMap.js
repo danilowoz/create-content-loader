3e8688f769406e7e6f720d2bb4b2310306287145
dagh <dagh@zhaw.ch>
2019-10-10 10:42:15 +0200

    add google share location with contact loader

--- src/Gallery/insertYourLoaderHere/GoogleMap.js
+++ src/Gallery/insertYourLoaderHere/GoogleMap.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
 const GoogleMap = props => {
   return (
@@ -11,13 +11,12 @@ const GoogleMap = props => {
       secondaryColor="#ecebeb"
       {...props}
     >
-      <circle cx="70" cy="50" r="30" /> 
+      <circle cx="70" cy="50" r="30" />
       <rect x="0" y="90" rx="0" ry="0" width="140" height="25" />
-      <circle cx="230" cy="50" r="30" /> 
+      <circle cx="230" cy="50" r="30" />
       <rect x="160" y="90" rx="0" ry="0" width="140" height="25" />
-      <circle cx="390" cy="50" r="30" /> 
+      <circle cx="390" cy="50" r="30" />
       <rect x="320" y="90" rx="0" ry="0" width="140" height="25" />
-      
     </ContentLoader>
   )
 }
@@ -25,8 +24,9 @@ const GoogleMap = props => {
 GoogleMap.metadata = {
   name: 'Ali Daghighi', // My name
   github: 'alidaghighi', // Github username
-  description: 'This is a sample of google map share location with contact loader', // Little tagline
-  filename: 'GoogleMap' // filename of your loader
+  description:
+    'This is a sample of google map share location with contact loader', // Little tagline
+  filename: 'GoogleMap', // filename of your loader
 }
 
 export default GoogleMap
