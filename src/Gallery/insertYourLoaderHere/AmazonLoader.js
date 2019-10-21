047d1892265bf87cafdb27cc152dabbaac81b7ea
Danilo Woznica <danilowoz@gmail.com>
2019-10-21 13:49:55 +0100

    fix(gallery): add new loaders

--- src/Gallery/insertYourLoaderHere/AmazonLoader.js
+++ src/Gallery/insertYourLoaderHere/AmazonLoader.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
 const AmazonLoader = props => {
   return (
@@ -11,33 +11,33 @@ const AmazonLoader = props => {
       secondaryColor="#ecebeb"
       {...props}
     >
-    <circle cx="472" cy="159" r="7" />
-    <rect x="487" y="154" rx="5" ry="5" width="220" height="10" />
-    <circle cx="472" cy="190" r="7" />
-    <rect x="487" y="184" rx="5" ry="5" width="220" height="10" />
-    <circle cx="472" cy="219" r="7" />
-    <rect x="487" y="214" rx="5" ry="5" width="220" height="10" />
-    <circle cx="472" cy="249" r="7" />
-    <rect x="487" y="244" rx="5" ry="5" width="220" height="10" />
-    <rect x="64" y="18" rx="0" ry="0" width="346" height="300" />
-    <rect x="229" y="300" rx="0" ry="0" width="0" height="0" />
-    <rect x="111" y="340" rx="0" ry="0" width="0" height="0" />
-    <rect x="121" y="342" rx="0" ry="0" width="0" height="0" />
-    <rect x="10" y="20" rx="0" ry="0" width="40" height="44" />
-    <rect x="10" y="80" rx="0" ry="0" width="40" height="44" />
-    <rect x="10" y="140" rx="0" ry="0" width="40" height="44" />
-    <rect x="194" y="329" rx="0" ry="0" width="0" height="0" />
-    <rect x="192" y="323" rx="0" ry="0" width="0" height="0" />
-    <rect x="185" y="323" rx="0" ry="0" width="0" height="0" />
-    <rect x="10" y="200" rx="0" ry="0" width="40" height="44" />
-    <rect x="470" y="18" rx="0" ry="0" width="300" height="25" />
-    <rect x="470" y="58" rx="0" ry="0" width="300" height="6" />
-    <rect x="470" y="68" rx="0" ry="0" width="300" height="6" />
-    <rect x="470" y="78" rx="0" ry="0" width="300" height="6" />
-    <rect x="798" y="135" rx="0" ry="0" width="0" height="0" />
-    <rect x="731" y="132" rx="0" ry="0" width="0" height="0" />
-    <rect x="470" y="99" rx="0" ry="0" width="70" height="30" />
-    <rect x="560" y="99" rx="0" ry="0" width="70" height="30" />
+      <circle cx="472" cy="159" r="7" />
+      <rect x="487" y="154" rx="5" ry="5" width="220" height="10" />
+      <circle cx="472" cy="190" r="7" />
+      <rect x="487" y="184" rx="5" ry="5" width="220" height="10" />
+      <circle cx="472" cy="219" r="7" />
+      <rect x="487" y="214" rx="5" ry="5" width="220" height="10" />
+      <circle cx="472" cy="249" r="7" />
+      <rect x="487" y="244" rx="5" ry="5" width="220" height="10" />
+      <rect x="64" y="18" rx="0" ry="0" width="346" height="300" />
+      <rect x="229" y="300" rx="0" ry="0" width="0" height="0" />
+      <rect x="111" y="340" rx="0" ry="0" width="0" height="0" />
+      <rect x="121" y="342" rx="0" ry="0" width="0" height="0" />
+      <rect x="10" y="20" rx="0" ry="0" width="40" height="44" />
+      <rect x="10" y="80" rx="0" ry="0" width="40" height="44" />
+      <rect x="10" y="140" rx="0" ry="0" width="40" height="44" />
+      <rect x="194" y="329" rx="0" ry="0" width="0" height="0" />
+      <rect x="192" y="323" rx="0" ry="0" width="0" height="0" />
+      <rect x="185" y="323" rx="0" ry="0" width="0" height="0" />
+      <rect x="10" y="200" rx="0" ry="0" width="40" height="44" />
+      <rect x="470" y="18" rx="0" ry="0" width="300" height="25" />
+      <rect x="470" y="58" rx="0" ry="0" width="300" height="6" />
+      <rect x="470" y="68" rx="0" ry="0" width="300" height="6" />
+      <rect x="470" y="78" rx="0" ry="0" width="300" height="6" />
+      <rect x="798" y="135" rx="0" ry="0" width="0" height="0" />
+      <rect x="731" y="132" rx="0" ry="0" width="0" height="0" />
+      <rect x="470" y="99" rx="0" ry="0" width="70" height="30" />
+      <rect x="560" y="99" rx="0" ry="0" width="70" height="30" />
     </ContentLoader>
   )
 }
@@ -46,7 +46,7 @@ AmazonLoader.metadata = {
   name: 'Sarah Ann Garcia',
   github: 'sgarcia30',
   description: 'This is an Amazon sample product loader.', // Little tagline
-  filename: 'AmazonLoader'
+  filename: 'AmazonLoader',
 }
 
 export default AmazonLoader
--- src/Gallery/insertYourLoaderHere/UpworkJobLoader.js
+++ src/Gallery/insertYourLoaderHere/UpworkJobLoader.js
@@ -3,18 +3,18 @@ import ContentLoader from 'react-content-loader'
 
 const UpworkJobLoader = props => {
   return (
-    <ContentLoader 
-        height={200}
-        width={800}
-        speed={2}
-        primaryColor="#f3f3f3"
-        secondaryColor="#ecebeb"
+    <ContentLoader
+      height={200}
+      width={800}
+      speed={2}
+      primaryColor="#f3f3f3"
+      secondaryColor="#ecebeb"
     >
-        <rect x="37" y="34" rx="0" ry="0" width="0" height="0" /> 
-        <rect x="28" y="29" rx="0" ry="0" width="258" height="32" /> 
-        <rect x="28" y="71" rx="0" ry="0" width="465" height="32" /> 
-        <rect x="434" y="94" rx="0" ry="0" width="0" height="0" /> 
-        <rect x="29" y="116" rx="0" ry="0" width="749" height="32" />
+      <rect x="37" y="34" rx="0" ry="0" width="0" height="0" />
+      <rect x="28" y="29" rx="0" ry="0" width="258" height="32" />
+      <rect x="28" y="71" rx="0" ry="0" width="465" height="32" />
+      <rect x="434" y="94" rx="0" ry="0" width="0" height="0" />
+      <rect x="29" y="116" rx="0" ry="0" width="749" height="32" />
     </ContentLoader>
   )
 }
