e26f23cf64c81d182637ab00b3f2d668da1ecb91
Royal Bhati <royal240596@gmail.com>
2019-10-23 08:34:10 -0400

    dev.to cardd (#102)

---
+++ src/Gallery/insertYourLoaderHere/DevtoCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const DevTo = () => (
+  <ContentLoader
+    height={475}
+    width={400}
+    speed={2}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+
+    <circle cx="30" cy="258" r="30" />
+    <rect x="75" y="233" rx="4" ry="4" width="100" height="13" />
+    <rect x="75" y="260" rx="4" ry="4" width="50" height="8" />
+    <rect x="0" y="210" rx="5" ry="5" width="400" height="10" />
+    <rect x="0" y="0" rx="5" ry="5" width="400" height="200" />
+  </ContentLoader>
+)
+
+DevtoCard.metadata = {
+  name: 'RoyalBhati',
+  github: 'royalbhati',
+  description: 'Dev.to card',
+  filename: 'DevtoCard',
+}
+
+export default DevTo
