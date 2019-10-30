166f89ad5d59efe3ccd235669940bc7959c4d802
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2019-10-30 07:12:54 -0300

    add banner ads - hacktoberfest (#105)

---
+++ src/Gallery/insertYourLoaderHere/BannerAds.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const BannerAds = () => (
+    <ContentLoader
+        height={250}
+        width={320}
+        speed={2}
+        primaryColor="#dfdcdc"
+        secondaryColor="#ecebeb"
+    >
+        <rect x="0" y="0" rx="3" ry="3" width="320" height="250" />
+    </ContentLoader>
+)
+
+BannerAds.metadata = {
+    name: 'Cristina Dias',
+    github: 'crisgit',
+    description: 'Simple Banner Ads for Phones and Tablets',
+    filename: 'BannerAds',
+}
+
+export default BannerAds
