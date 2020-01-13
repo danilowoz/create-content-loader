22873f899ebe0d336c83022f97da28f344d387d0
swatana3 <swatanabe12721@gmail.com>
2020-01-13 05:52:41 -0800

    Create PieChart.js (#124)
    Makes PieChart loader!

---
+++ src/Gallery/insertYourLoaderHere/PieChart.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const PieChart = props => {
+  return (
+    <ContentLoader height={200} width={400} speed={2} {...props}>
+      <rect x="100" y="5" rx="0" ry="0" width="200" height="15" />
+      <circle cx="140" cy="110" r="70" />
+      <rect x="230" y="50" rx="0" ry="0" width="7" height="7" />
+      <rect x="250" y="50" rx="0" ry="0" width="30" height="7" />
+      <rect x="230" y="64" rx="0" ry="0" width="7" height="7" />
+      <rect x="250" y="64" rx="0" ry="0" width="30" height="7" />
+      <rect x="230" y="78" rx="0" ry="0" width="7" height="7" />
+      <rect x="250" y="78" rx="0" ry="0" width="30" height="7" />
+      <rect x="230" y="92" rx="0" ry="0" width="7" height="7" />
+      <rect x="250" y="92" rx="0" ry="0" width="30" height="7" />
+    </ContentLoader>
+  )
+}
+
+PieChart.metadata = {
+  name: 'Sarah Watanabe', //Names
+  github: 'swatana3', //github username
+  description: 'This is a pie chart loader.', // Little tagline
+  filename: 'PieChart', //filename
+}
+
+export default PieChart
