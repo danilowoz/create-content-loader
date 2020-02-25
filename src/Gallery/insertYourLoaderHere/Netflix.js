5e5f4788e11777be5f04becfc5208e10f7775a51
Danilo Woznica <danilowoz@gmail.com>
2020-02-25 10:07:20 +0000

    fix(gallery): adds netflix

--- src/Gallery/insertYourLoaderHere/Netflix.js
+++ src/Gallery/insertYourLoaderHere/Netflix.js
@@ -1,19 +1,23 @@
-import React from "react";
-import ContentLoader from "react-content-loader";
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
 const NetflixLoader = props => {
-  
-  //Get values from props
-  //const { rows, columns, coverHeight, coverWidth, padding, speed } = props;
-  
-  //Hardcoded values
-  const { rows, columns, coverHeight, coverWidth, padding, speed } = { 2, 5, 85, 65, 5, 1 };
-  
-  const coverHeightWithPadding = coverHeight + padding;
-  const coverWidthWithPadding = coverWidth + padding;
-  const initial = 35;
-  const covers = Array(columns * rows).fill(1);
-  
+  // Get values from props
+  // const { rows, columns, coverHeight, coverWidth, padding, speed } = props;
+
+  // Hardcoded values
+  const rows = 2
+  const columns = 5
+  const coverHeight = 85
+  const coverWidth = 65
+  const padding = 5
+  const speed = 1
+
+  const coverHeightWithPadding = coverHeight + padding
+  const coverWidthWithPadding = coverWidth + padding
+  const initial = 35
+  const covers = Array(columns * rows).fill(1)
+
   return (
     <ContentLoader
       speed={speed}
@@ -22,12 +26,18 @@ const NetflixLoader = props => {
       primaryColor="#242b34"
       secondaryColor="#343d4c"
     >
-      <rect x="0" y="0" rx="0" ry="0" width={columns * coverWidthWithPadding - padding} height="20" />
+      <rect
+        x="0"
+        y="0"
+        rx="0"
+        ry="0"
+        width={columns * coverWidthWithPadding - padding}
+        height="20"
+      />
 
       {covers.map((g, i) => {
-        let vy = Math.floor(i / columns) * coverHeightWithPadding + initial;
-        let vx =
-          (i * coverWidthWithPadding) % (columns * coverWidthWithPadding);
+        let vy = Math.floor(i / columns) * coverHeightWithPadding + initial
+        let vx = (i * coverWidthWithPadding) % (columns * coverWidthWithPadding)
         return (
           <rect
             x={vx}
@@ -37,17 +47,17 @@ const NetflixLoader = props => {
             width={coverWidth}
             height={coverHeight}
           />
-        );
+        )
       })}
     </ContentLoader>
-  );
-};
-
-NetflixLoader.metadata = {
-  name: 'Pratik Pathak', 
-  github: 'PathakPratik', 
-  description: 'Netflix Style Dynamic Loader', 
-  filename: 'Netflix.js' 
+  )
 }
 
-export default NetflixLoader;
+NetflixLoader.metadata = {
+  name: 'Pratik Pathak',
+  github: 'PathakPratik',
+  description: 'Netflix Style Dynamic',
+  filename: 'Netflix',
+}
+
+export default NetflixLoader
