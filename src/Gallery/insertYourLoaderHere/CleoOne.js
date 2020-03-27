40a2bc0a6df0d6b3cc8e3802c79968d93dd603cd
Yusuf Özlü <ozluyusuf@gmail.com>
2020-03-27 13:18:52 +0100

    Create CleoOne.js

---
+++ src/Gallery/insertYourLoaderHere/CleoOne.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const CleoOne = ({ style, ...rest }) => (
+  <ContentLoader
+    style={{
+      ...{
+        background: "#fff",
+        borderRadius: 4,
+        boxShadow: "0 4px 24px 0 rgba(49, 73, 177, 0.15)"
+      },
+      ...style
+    }}
+    {...rest}
+    height="230"
+    width="265"
+    viewBox="0 0 265 230"
+  >
+    <rect x="15" y="15" rx="4" ry="4" width="200" height="25" />
+    <rect x="15" y="50" rx="2" ry="2" width="40" height="15" />
+    <rect x="75" y="45" rx="16" ry="16" width="55" height="22" />
+    <rect x="15" y="75" rx="3" ry="3" width="215" height="15" />
+    <rect x="15" y="105" rx="3" ry="3" width="50" height="15" />
+    <rect x="75" y="105" rx="3" ry="3" width="50" height="15" />
+    <rect x="135" y="105" rx="3" ry="3" width="50" height="15" />
+    <rect x="15" y="135" rx="16" ry="16" width="55" height="22" />
+    <rect x="15" y="165" rx="2" ry="2" width="150" height="50" />
+    <rect x="215" y="180" rx="2" ry="2" width="40" height="20" />
+  </ContentLoader>
+);
+
+CleoOne.metadata = {
+  name: "Yusuf Özlü",
+  github: "ozluy",
+  description: "Dashboard strategy item on CLEO.one(https://cleo.one).",
+  filename: "CleoOne"
+};
+
+export default CleoOne;
