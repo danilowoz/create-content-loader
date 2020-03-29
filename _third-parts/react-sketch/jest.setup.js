099efc408ce15ea2f7cbf7ecba9c16693471bb1c
Danilo Woznica <danilowoz@gmail.com>
2020-03-29 18:16:10 +0100

    Remove unnecessary code from dependency

--- _third-parts/react-sketch/jest.setup.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, no-multi-assign, no-undef */
-import 'jest-enzyme'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-Enzyme.configure({
-  adapter: new Adapter(),
-})
