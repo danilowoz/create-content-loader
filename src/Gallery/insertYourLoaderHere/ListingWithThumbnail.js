ca5902c9a0041f1d10544872de3eac9a9cf55ca3
Danilo Woznica <danilowoz@gmail.com>
2020-04-01 12:18:32 +0100

    Update gallery

--- src/Gallery/insertYourLoaderHere/ListingWithThumbnail.js
+++ src/Gallery/insertYourLoaderHere/ListingWithThumbnail.js
@@ -1,34 +1,34 @@
-import React from "react";
-import ContentLoader from "react-content-loader";
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
 const ListingWithThumbnail = props => {
   return (
     <ContentLoader
-      height={40}
+      height={1200}
       width={1060}
       speed={2}
       primaryColor="#d9d9d9"
       secondaryColor="#ecebeb"
       {...props}
     >
-      <Rect x="103" y="12" rx="3" ry="3" width="123" height="7" />
-      <Rect x="102" y="152" rx="3" ry="3" width="171" height="6" />
-      <Circle cx="44" cy="42" r="38" />
-      <Circle cx="44" cy="147" r="38" />
-      <Circle cx="44" cy="251" r="38" />
-      <Rect x="105" y="117" rx="3" ry="3" width="123" height="7" />
-      <Rect x="104" y="222" rx="3" ry="3" width="123" height="7" />
-      <Rect x="105" y="48" rx="3" ry="3" width="171" height="6" />
-      <Rect x="104" y="257" rx="3" ry="3" width="171" height="6" />
+      <rect x="103" y="12" rx="3" ry="3" width="123" height="7" />
+      <rect x="102" y="152" rx="3" ry="3" width="171" height="6" />
+      <circle cx="44" cy="42" r="38" />
+      <circle cx="44" cy="147" r="38" />
+      <circle cx="44" cy="251" r="38" />
+      <rect x="105" y="117" rx="3" ry="3" width="123" height="7" />
+      <rect x="104" y="222" rx="3" ry="3" width="123" height="7" />
+      <rect x="105" y="48" rx="3" ry="3" width="171" height="6" />
+      <rect x="104" y="257" rx="3" ry="3" width="171" height="6" />
     </ContentLoader>
-  );
-};
+  )
+}
 
 ListingWithThumbnail.metadata = {
-  name: "Rituraj ratan",
-  github: "riturajratan",
-  description: "Listing with thumbnail",
-  filename: "ListingWithThumbnail"
-};
+  name: 'Rituraj ratan',
+  github: 'riturajratan',
+  description: 'Listing with thumbnail',
+  filename: 'ListingWithThumbnail',
+}
 
-export default ListingWithThumbnail;
+export default ListingWithThumbnail
--- src/Gallery/insertYourLoaderHere/NotificationLoader.js
+++ src/Gallery/insertYourLoaderHere/NotificationLoader.js
@@ -3,49 +3,49 @@ import ContentLoader from 'react-content-loader'
 
 const NotificationLoader = props => {
   return (
-    <ContentLoader 
-    speed={2}
-    width={350}
-    height={600}
-    viewBox="0 0 350 600"
-    backgroundColor="#f5f5f5"
-    foregroundColor="#dbdbdb"
-    {...props}
-  >
-    <rect x="4" y="8" rx="3" ry="3" width="8" height="570" /> 
-    <rect x="5" y="573" rx="3" ry="3" width="331" height="7" /> 
-    <rect x="329" y="9" rx="3" ry="3" width="8" height="570" /> 
-    <rect x="102" y="69" rx="3" ry="3" width="102" height="7" /> 
-    <rect x="92" y="47" rx="3" ry="3" width="178" height="6" /> 
-    <circle cx="48" cy="63" r="18" /> 
-    <rect x="95" y="95" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="105" y="169" rx="3" ry="3" width="102" height="7" /> 
-    <rect x="95" y="147" rx="3" ry="3" width="178" height="6" /> 
-    <circle cx="51" cy="163" r="18" /> 
-    <rect x="98" y="195" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="107" y="265" rx="3" ry="3" width="102" height="7" /> 
-    <rect x="97" y="243" rx="3" ry="3" width="178" height="6" /> 
-    <circle cx="53" cy="259" r="18" /> 
-    <rect x="100" y="291" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="108" y="365" rx="3" ry="3" width="102" height="7" /> 
-    <rect x="98" y="343" rx="3" ry="3" width="178" height="6" /> 
-    <circle cx="54" cy="359" r="18" /> 
-    <rect x="101" y="391" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="110" y="458" rx="3" ry="3" width="102" height="7" /> 
-    <rect x="100" y="436" rx="3" ry="3" width="178" height="6" /> 
-    <circle cx="56" cy="452" r="18" /> 
-    <rect x="103" y="484" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="114" y="507" rx="3" ry="3" width="102" height="7" /> 
-    <rect x="103" y="534" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="5" y="8" rx="3" ry="3" width="331" height="7" />
-  </ContentLoader>
+    <ContentLoader
+      speed={2}
+      width={350}
+      height={600}
+      viewBox="0 0 350 600"
+      backgroundColor="#f5f5f5"
+      foregroundColor="#dbdbdb"
+      {...props}
+    >
+      <rect x="4" y="8" rx="3" ry="3" width="8" height="570" />
+      <rect x="5" y="573" rx="3" ry="3" width="331" height="7" />
+      <rect x="329" y="9" rx="3" ry="3" width="8" height="570" />
+      <rect x="102" y="69" rx="3" ry="3" width="102" height="7" />
+      <rect x="92" y="47" rx="3" ry="3" width="178" height="6" />
+      <circle cx="48" cy="63" r="18" />
+      <rect x="95" y="95" rx="3" ry="3" width="178" height="6" />
+      <rect x="105" y="169" rx="3" ry="3" width="102" height="7" />
+      <rect x="95" y="147" rx="3" ry="3" width="178" height="6" />
+      <circle cx="51" cy="163" r="18" />
+      <rect x="98" y="195" rx="3" ry="3" width="178" height="6" />
+      <rect x="107" y="265" rx="3" ry="3" width="102" height="7" />
+      <rect x="97" y="243" rx="3" ry="3" width="178" height="6" />
+      <circle cx="53" cy="259" r="18" />
+      <rect x="100" y="291" rx="3" ry="3" width="178" height="6" />
+      <rect x="108" y="365" rx="3" ry="3" width="102" height="7" />
+      <rect x="98" y="343" rx="3" ry="3" width="178" height="6" />
+      <circle cx="54" cy="359" r="18" />
+      <rect x="101" y="391" rx="3" ry="3" width="178" height="6" />
+      <rect x="110" y="458" rx="3" ry="3" width="102" height="7" />
+      <rect x="100" y="436" rx="3" ry="3" width="178" height="6" />
+      <circle cx="56" cy="452" r="18" />
+      <rect x="103" y="484" rx="3" ry="3" width="178" height="6" />
+      <rect x="114" y="507" rx="3" ry="3" width="102" height="7" />
+      <rect x="103" y="534" rx="3" ry="3" width="178" height="6" />
+      <rect x="5" y="8" rx="3" ry="3" width="331" height="7" />
+    </ContentLoader>
   )
 }
 
 NotificationLoader.metadata = {
   name: 'Sridhar Easwaran',
   github: 'sridhareaswaran',
-  description: 'Loader for Notification section',
+  description: 'Notification section',
   filename: 'NotificationLoader',
 }
 
