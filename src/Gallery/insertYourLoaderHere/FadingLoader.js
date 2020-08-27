bfaa7d5a3c7ea95de13193743aac9994d96f223d
Nikhil-KNOLSKAPE <anandnikhil91@gmail.com>
2020-08-28 00:41:25 +0530

    Add multiple faded cards in Loader

--- src/Gallery/insertYourLoaderHere/FadingLoader.js
+++ src/Gallery/insertYourLoaderHere/FadingLoader.js
@@ -1,29 +1,103 @@
 import React from 'react'
 import ContentLoader from 'react-content-loader'
 
-const FadedLoader = props => {
+const FadingLoader = props => {
+  return(
+    <div>
+      <FadingLoaderCard1 /> 
+      <FadingLoaderCard2 /> 
+      <FadingLoaderCard3 />
+      <FadingLoaderCard4 />
+      <FadingLoaderCard5 />
+    </div>
+  )
+}
+
+const FadingLoaderCard1 = props => {
   return (
     <ContentLoader
       width={400}
-      height={1600}
+      height={40}
       speed={2}
-      viewBox="0 0 400 1600"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#dcdbdb"
-      {...props}
+      backgroundColor="#ababab"
+      foregroundColor="#fafafa"
     >
-      <rect x="90" y="16" rx="5" ry="5" width="300" height="15" />
-      <rect x="129" y="39" rx="5" ry="5" width="220" height="9" />
-      <rect x="26" y="10" rx="0" ry="0" width="50" height="45" />
+      <rect x="70" y="15" rx="5" ry="5" width="300" height="15" />
+      <rect x="70" y="39" rx="5" ry="5" width="220" height="9" />
+      <rect x="20" y="10" rx="0" ry="0" width="40" height="40" />
     </ContentLoader>
   )
 }
 
-FadedLoader.metadata = {
+const FadingLoaderCard2 = props => {
+  return (
+    <ContentLoader
+      width={400}
+      height={40}
+      speed={2}
+      backgroundColor="#bfbfbf"
+      foregroundColor="#fafafa"
+    >
+      <rect x="70" y="15" rx="5" ry="5" width="300" height="15" />
+      <rect x="70" y="39" rx="5" ry="5" width="220" height="9" />
+      <rect x="20" y="10" rx="0" ry="0" width="40" height="40" />
+    </ContentLoader>
+  )
+}
+
+const FadingLoaderCard3 = props => {
+  return (
+    <ContentLoader
+      width={400}
+      height={40}
+      speed={2}
+      backgroundColor="#dadada"
+      foregroundColor="#fafafa"
+    >
+      <rect x="70" y="15" rx="5" ry="5" width="300" height="15" />
+      <rect x="70" y="39" rx="5" ry="5" width="220" height="9" />
+      <rect x="20" y="10" rx="0" ry="0" width="40" height="40" />
+    </ContentLoader>
+  )
+}
+
+const FadingLoaderCard4 = props => {
+  return (
+    <ContentLoader
+      width={400}
+      height={40}
+      speed={2}
+      backgroundColor="#ececec"
+      foregroundColor="#fafafa"
+    >
+      <rect x="70" y="15" rx="5" ry="5" width="300" height="15" />
+      <rect x="70" y="39" rx="5" ry="5" width="220" height="9" />
+      <rect x="20" y="10" rx="0" ry="0" width="40" height="40" />
+    </ContentLoader>
+  )
+}
+
+const FadingLoaderCard5 = props => {
+  return (
+    <ContentLoader
+      width={400}
+      height={40}
+      speed={2}
+      backgroundColor="#f7f7f7"
+      foregroundColor="#fafafa"
+    >
+      <rect x="70" y="15" rx="5" ry="5" width="300" height="15" />
+      <rect x="70" y="39" rx="5" ry="5" width="220" height="9" />
+      <rect x="20" y="10" rx="0" ry="0" width="40" height="40" />
+    </ContentLoader>
+  )
+}
+
+FadingLoader.metadata = {
   name: 'Nikhil Anand', // My name
   github: 'anandnkhl', // Github username
   description: 'Loader that fades downwards', // Little tagline
-  filename: 'FadedLoader', // filename of your loader
+  filename: 'FadingLoader', // filename of your loader
 }
 
-export default FadedLoader
+export default FadingLoader
