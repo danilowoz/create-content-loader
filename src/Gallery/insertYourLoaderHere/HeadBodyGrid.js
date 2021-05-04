5fb5bbb5e4037c0a4378a02d11246e37de49cf54
Danilo Woznica <danilowoz@gmail.com>
2021-05-04 20:25:40 +0100

    Update HeadBodyGrid.js

--- src/Gallery/insertYourLoaderHere/HeadBodyGrid.js
+++ src/Gallery/insertYourLoaderHere/HeadBodyGrid.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ContentLoader from 'react-content-loader'
 
 const HeadBodyGrid = ({ ...rest }) => (
-  <ContentLoader height="500" width="1000" backgroundColor="#fff"
-  foregroundColor="#f6f5f5" viewBox="0 0 265 230" {...rest}>
+  <ContentLoader height="500" width="1000" viewBox="0 0 265 230" {...rest}>
     <rect x="15" y="15" rx="4" ry="4" width="350" height="25" />
     <rect x="15" y="50" rx="2" ry="2" width="350" height="150" />
     <rect x="15" y="230" rx="2" ry="2" width="170" height="20" />
     <rect x="60" y="230" rx="2" ry="2" width="170" height="20" />
   </ContentLoader>
 )
+
 HeadBodyGrid.metadata = {
     name: 'Didier Munezero',
     github: 'didiermunezero',
