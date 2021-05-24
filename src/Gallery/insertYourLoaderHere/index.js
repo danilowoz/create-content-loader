3924c529da940fde70955e81940c77d60677130b
arfath77 <arfathtade@gmail.com>
2021-05-25 00:35:52 +0530

    add global sidebar loader

--- src/Gallery/insertYourLoaderHere/index.js
+++ src/Gallery/insertYourLoaderHere/index.js
@@ -1,3 +1,4 @@
+export { default as GlobalSidebar } from './GlobalSidebar'
 export { default as HeadBodyGrid } from './HeadBodyGrid'
 export { default as ResponsiveArticle } from './ResponsiveArticle'
 export { default as BootstrapCardDataTable } from './BootstrapCardDataTable'
