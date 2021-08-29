254b7dd7f6b1238ac0a01d3d4888c8dcad69c1a2
Danilo Woznica <danilowoz@gmail.com>
2021-08-29 18:49:29 +0100

    fix(example): wrap in a g tag

--- src/Gallery/insertYourLoaderHere/RepeatableTableRows.js
+++ src/Gallery/insertYourLoaderHere/RepeatableTableRows.js
@@ -7,15 +7,14 @@ const RepeatableTableRows = props => {
 
   return (
     <ContentLoader viewBox={`0 0 1500 ${rowHeight * rows}`} {...props}>
-      {[...Array(rows)].map((el, index) => {
+      {new Array(rows).fill(' ').map((el, index) => {
         const contentVerticalPosition = contentHeight =>
           rows > 1 ? contentHeight + rowHeight * index : contentHeight
-
         return (
           <Fragment key={index}>
             <rect
               x="20"
-              y={contentVerticalPosition(20)}
+              y={`${contentVerticalPosition(20)}`}
               rx="4"
               ry="4"
               width="40"
@@ -23,7 +22,7 @@ const RepeatableTableRows = props => {
             />
             <rect
               x="100"
-              y={contentVerticalPosition(20)}
+              y={`${contentVerticalPosition(20)}`}
               rx="10"
               ry="4"
               width="600"
@@ -31,7 +30,7 @@ const RepeatableTableRows = props => {
             />
             <rect
               x="750"
-              y={contentVerticalPosition(20)}
+              y={`${contentVerticalPosition(20)}`}
               rx="10"
               ry="4"
               width="600"
@@ -39,14 +38,14 @@ const RepeatableTableRows = props => {
             />
             <rect
               x="1450"
-              y={contentVerticalPosition(20)}
+              y={`${contentVerticalPosition(20)}`}
               rx="4"
               ry="4"
               width="20"
               height="20"
             />
             <rect
-              y={contentVerticalPosition(59)}
+              y={`${contentVerticalPosition(59)}`}
               x="10"
               ry="10"
               width="1500"
@@ -62,7 +61,7 @@ RepeatableTableRows.metadata = {
   name: 'Lukas Werner',
   github: 'sherpaPSX',
   description:
-    'Repeatable table rows. You can easly define number of rows in props and generate then in one svg',
+    'Repeatable table rows. You can easily define number of rows in props and generate then in one svg',
   filename: 'RepeatableTableRows',
 }
 
